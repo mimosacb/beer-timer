610989de362dc44b768c00fb54cdbe41fe467602
chowlett <chris.t.howlett@gmail.com>
2016-03-31 13:14:56 -0400

    Added hours to timer
    Made the shared timer also display hours. Added padding for both
    minutes and hours.

--- shared/components/timer/timer.jsx
+++ shared/components/timer/timer.jsx
@@ -8,16 +8,18 @@ var Timer = React.createClass({
 			time : 0 //in seconds
 		};
 	},
+	getHours : function(){
+		return _.padStart(Math.floor(this.props.time/(60*60)), 2, '0');
+	},
 	getMinutes : function(){
-		return Math.floor(this.props.time/60);
+		return _.padStart(Math.floor((this.props.time/60) % 60), 2, '0');
 	},
 	getSeconds : function(){
 		return _.padStart(this.props.time % 60, 2, '0');
 	},
-
 	render : function(){
 		return <div className='timer'>
-			{this.getMinutes()}:{this.getSeconds()}
+			{this.getHours()}:{this.getMinutes()}:{this.getSeconds()}
 		</div>
 	}
 });
