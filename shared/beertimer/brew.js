7d3fff9cf1c1a4eb9402af8dd4bfcd0c2c29bb3d
chowlett <chris.t.howlett@gmail.com>
2016-04-13 13:49:43 -0400

    Generate default steps cleaner

--- shared/beertimer/brew.js
+++ shared/beertimer/brew.js
@@ -1,45 +1,22 @@
-var defaultSteps = [
-	{
-		"name" : "setup",
+var makeDefaultStep = function(name, bgColor) {
+	return {
+		"name" : name,
 		"time" : 0,
 		"isCountDown" : false,
-		"bgColor" : "#81A801",
-	},
-	{
-		"name" : "mash",
-		"time" : 0,
-		"isCountDown" : false,
-		"bgColor" : "#D98027"
-	},
-	{
-		"name" : "sparge",
-		"time" : 0,
-		"isCountDown" : false,
-		"bgColor" : "#B36615"
-	},
-	{
-		"name" : "boil",
-		"time" : 0,
-		"isCountDown" : false,
-		"bgColor" : "#B64240"
-	},
-	{
-		"name" : "ice bath",
-		"time" : 0,
-		"isCountDown" : false,
-		"bgColor" : "#3EA0B5"
-	},
-	{
-		"name": "pitch",
-		"time": 0,
-		"isCountDown": false,
-		"bgColor": "#000000"
-	}
-];
+		"bgColor" : bgColor
+	};
+}
 
 var Brew = function(brewJson) {
 	this.name = brewJson && brewJson.name || "Default Brew";
-	this.steps = defaultSteps;
+	this.steps = [
+		makeDefaultStep("setup", "#81A801"),
+		makeDefaultStep("mash", "#D98027"),
+		makeDefaultStep("sparge", "#B36615"),
+		makeDefaultStep("boil", "#B64240"),
+		makeDefaultStep("ice bath", "#3EA0B5"),
+		makeDefaultStep("pitch", "#000000")
+	];
 
 	var recipe = brewJson && brewJson.recipe;
 	if (recipe) {
